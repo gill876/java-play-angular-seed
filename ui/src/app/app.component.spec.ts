b5653bc5e522a4f71e4a98da697f0f1084a0fd5e
Cargill Seiveright <microcargill@hotmail.com>
2021-02-08 02:48:18 -0500

    Again... Merge

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -1,52 +1,35 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { AppService } from './app.service';
-
-import { of } from 'rxjs';
-
-class FakeAppService extends AppService {
-  getWelcomeMessage() {
-    return of({
-      content: 'Test content'
-    });
-  }
-}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
-      imports: [
-        HttpClientModule,
-        RouterTestingModule
-      ]
-    }).overrideComponent(AppComponent, {
-      set: {
-        providers: [
-          { provide: AppService, useClass: FakeAppService}
-        ]
-      }
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
+  });
+
+  it(`should have as title 'angular-crash-todolist'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Test content');
-  }));
-  it('should render title in a h1 tag', async(() => {
+    expect(app.title).toEqual('angular-crash-todolist');
+  });
+
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Test content!');
-  }));
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-crash-todolist!');
+  });
 });
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,53 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouteExampleComponent } from './route-example/route-example.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
-import { AppService } from './app.service';
 import { AppHttpInterceptorService } from './http-interceptor.service';
-
-const routes: Routes = [
-  {
-    path: 'java',
-    component: RouteExampleComponent,
-    data: { technology: 'Java' }
-  },
-  {
-    path: 'play',
-    component: RouteExampleComponent,
-    data: { technology: 'Play' }
-  },
-  {
-    path: 'angular',
-    component: RouteExampleComponent,
-    data: { technology: 'Angular' }
-  },
-  {
-    path: '**',
-    redirectTo: '/play',
-    pathMatch: 'full'
-  }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RouteExampleComponent
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     HttpClientXsrfModule.withOptions({
       cookieName: 'Csrf-Token',
       headerName: 'Csrf-Token',
-    }),
-    RouterModule.forRoot(routes)
+    })
   ],
   providers: [
-    AppService,
     {
       multi: true,
       provide: HTTP_INTERCEPTORS,
